d66f10bc58959dd00ba87eeb5d061cebe942fb2f
mainangaruiya <003paulmaina@gmail.com>
2023-03-15 15:58:43 +0300

    import & modules

--- 0x02-python-import_modules/5-variable_load.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 4-hidden_discovery.py
-
-if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-
-    print(a)
